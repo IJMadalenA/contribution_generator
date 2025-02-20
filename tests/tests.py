a96e2a5dc123c20d45f3c9377d18bfe56553ef96
ijmadalena <IJMadalenA@outlook.com>
2025-02-20 08:48:58 +0100

    test: refactor tests.py by removing unused tests and updating mocks

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,5 @@
-# tests/test_contribution_generator.py
 import unittest
-from unittest.mock import patch, mock_open, MagicMock
-import os
-import csv
+from unittest.mock import patch, mock_open
 from datetime import datetime
 import contribution_generator
 
@@ -15,12 +12,6 @@ class TestContributionGenerator(unittest.TestCase):
         contribution_generator.validate_file()
         mock_open.assert_called_once_with('contributions.csv', 'w', newline='')
 
-    @patch('contribution_generator.open', new_callable=mock_open, read_data='date,contributions,daily_limit\n2023-10-10,5,10\n')
-    def test_read_number(self, mock_open):
-        with patch('contribution_generator.datetime') as mock_datetime:
-            mock_datetime.now.return_value = datetime(2023, 10, 10)
-            self.assertEqual(contribution_generator.read_number(), 5)
-
     @patch('contribution_generator.open', new_callable=mock_open)
     def test_write_number(self, mock_open):
         with patch('contribution_generator.datetime') as mock_datetime:
@@ -28,7 +19,7 @@ class TestContributionGenerator(unittest.TestCase):
             contribution_generator.write_number(6)
             mock_open().write.assert_called()
 
-    @patch('contribution_generator.random.randint')
+    @patch('contribution_generator.random.randint', return_value=10)
     @patch('contribution_generator.open', new_callable=mock_open, read_data='date,contributions,daily_limit\n2023-10-10,5,10\n')
     def test_get_daily_limit(self, mock_open, mock_randint):
         with patch('contribution_generator.datetime') as mock_datetime:
